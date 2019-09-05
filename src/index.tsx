98b378333d2df0460a503423eb8b90108b3a956c
JulianG <juliangaramendy@gmail.com>
2019-09-05 20:46:43 +0100

    add prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<div>Hello</div>, document.getElementById('root'));
+ReactDOM.render(<div>Hello</div>, document.getElementById("root"));
