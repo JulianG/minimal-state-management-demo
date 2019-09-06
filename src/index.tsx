c8218e75fd9266cd322eac6d1de05b6a117ad47e
JulianG <juliangaramendy@gmail.com>
2019-09-06 05:51:22 +0100

    Fetching list of games from REST API.

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-ReactDOM.render(<div>Hello</div>, document.getElementById("root"));
+fetch('http://localhost:3001/games/')
+  .then(response => response.json())
+  .then(games => {
+    ReactDOM.render(
+      <pre>{JSON.stringify(games, null, 2)}</pre>,
+      document.getElementById('root')
+    );
+  });
